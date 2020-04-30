b783a4f6ef46579180d89d907f102c8e62733c90
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-04-30 11:10:07 +0200

    added css sass and styled the homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import HomePage from './homepage';
 
 class App extends React.Component {
 	render() {
 		return(
 			<div>
-				<p>Hello World</p>
+				<HomePage />
 			</div>
 		)
 	};
