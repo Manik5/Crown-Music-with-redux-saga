f59d177bf14d53fd1b228efb170e95c846637933
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-04-30 14:10:21 +0200

    added style to the item menu

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import HomePage from './pages/homepage/homepage'
+import HomePage from './pages/homepage/homepage';
+import './App.css';
 class App extends React.Component {
 	render() {
 		return(
--- src/components/menu-item/MenuItem.jsx
+++ src/components/menu-item/MenuItem.jsx
@@ -2,13 +2,21 @@ import React from 'react';
 
 import './menuitem.scss';
 
-export const MenuItem = ({ title }) => (
-	<div className="menu-item">
-		<div className="content">
-			<h1 className="title">{ title }</h1>
-			<span className="subtitle">Shop Now</span>
-		</div>
-	</div>
-)
+export const MenuItem = ({ imageUrl, size, title }) => (
+         <div className={` ${size} menu-item`}>
+           <div
+             className="background-image"
+             style={{
+               backgroundImage: `url(${imageUrl})`,
+             }}
+           />
+           <div className="content">
+             <h1 className="title">{title}</h1>
+             <span className="subtitle">Shop Now</span>
+           </div>
+         </div>
+       );
 
 export default MenuItem;
+
+//  In here it will pass all the props defined in  the Directory.jsx
