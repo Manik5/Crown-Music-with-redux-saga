a264026201e787d17033a39f5de698985ceff36f
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-05-01 22:41:36 +0200

    added inverted function to invert the color in the custombutton component

--- src/components/custombutton/CustomButton.jsx
+++ src/components/custombutton/CustomButton.jsx
@@ -3,10 +3,13 @@ import React from 'react';
 import './custombutton.scss';
 
 //  children is the "Sign In" between the CustomButton component in SignIn.jsx
-const CustomButton = ({ children,  isGoogleSignIn, ...otherProps }) => (
+//  inverted is the props to change the color
+const CustomButton = ({ children,  isGoogleSignIn, inverted, ...otherProps }) => (
 	<button
-		className= {`${isGoogleSignIn ?  'google-sign-in' : ' '} custom-button` }
-		{...otherProps}
+		className= { `${inverted  ? 'inverted' :  ' '} ${
+			isGoogleSignIn  ?  'google-sign-in' :  ' '
+			} custom-button` }
+			{...otherProps}
 		>
 		{children}
 	</button>
