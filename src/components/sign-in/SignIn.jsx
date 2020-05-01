437fd37bbdd4dcd3fabe72bd394c9c0c5b439574
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-05-01 16:25:27 +0200

    solved problem in signup

--- src/components/sign-in/SignIn.jsx
+++ src/components/sign-in/SignIn.jsx
@@ -3,7 +3,7 @@ import FormInput from '../form-input/FormInput';
 import CustomButton from '../custombutton/CustomButton';
 
 // firebase authentication with Google
-import { signInWithGoogle } from '../../firebase/firebase.utils';
+import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './signin.scss';
 
@@ -16,11 +16,18 @@ class SignIn extends React.Component {
 		}
 	}
 
-	handleSubmit = event => {
+	handleSubmit = async event => {
 		event.preventDefault();
 
-		this.setState({ email: "", password: "" })
-	}
+		const { email, password } = this.state;
+
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: "", password: ""});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
 	handleChange = event => {
 		const { value, name } = event.target;
