3b5e7c36a6348a29ccfaeb3fdf0a0254d36966c2
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-05-02 22:50:32 +0200

    matching the id of each category with its corresponding route

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 
 import CollectionItem from '../../components/collections-item/CollectionItem';
 
+// importing redux
+import { connect } from 'react-redux';
+//  defining the route for each category
+import { selectCollection } from '../../redux/shop/shop.selector';
+
 import './collection.scss';
 
-const CollectionPage = ({ match }) => {
+const CollectionPage = ({ collection }) => {
+	console.log(collection)
 	return (
-		<div className="category">
+		<div className="collection-page">
 			<h2>COLLECTION PAGE</h2>
 		</div>
 	);
 };
 
-export default CollectionPage;
+//  defining the route for each category
+const mapStateToProps = (state, ownProps ) => ({
+	collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(
+	mapStateToProps
+) (CollectionPage);
--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,8 +1,27 @@
 import { createSelector } from 'reselect';
 
+//  defining the route for each category
+const COLLECTION_ID_MAP = {
+	drums: 1,
+	piano: 2,
+	bassguitars: 3,
+	violins: 4,
+	guitars: 5
+}
+
+//  defining the route for each category
+export const selectCollection = collectionUrlParam =>
+	createSelector(
+		[selectCollections],
+		collections =>collections.find(
+			collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
+	);
+
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
 	[selectShop],
 	shop => shop.collections
 );
+
+
