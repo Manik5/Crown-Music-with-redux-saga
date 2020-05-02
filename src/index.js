93306942fe54e2844d9535d9cc2824f56a469ac6
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-05-02 18:07:41 +0200

    added redux persistor

--- src/index.js
+++ src/index.js
@@ -5,16 +5,22 @@ import App from './App';
 //  importing react router dom
 import { BrowserRouter } from 'react-router-dom';
 
+// importing redux persist
+
+import { PersistGate } from 'redux-persist/integration/react';
 //  importing Redux
 import { Provider } from 'react-redux';
-import store from './redux/store';
+import { store, persistor } from './redux/store';
 
 
 ReactDOM.render(
 	//  importing Redux by wrapping all in Provider
 	<Provider store={store}>
 		<BrowserRouter>
-			<App />
+		{/* importing redux-persist */}
+			<PersistGate persistor={persistor}>
+					<App />
+			</PersistGate>
 		</BrowserRouter>
 	</Provider>,
 	//  It will be wrapping the  App.js component
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from 'redux';
+// importing redux persist
+import { persistStore } from 'redux-persist';
+
 import logger from 'redux-logger';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// importing redux persist
+export const persistor = persistStore(store);
 
+export default { store, persistor };
