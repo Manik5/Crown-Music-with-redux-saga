9cc526b4817b6af856a9cb760fbfddf8b8f2d25a
Manik A A Ngurah <manik.aangurah@gmail.com>
2020-05-03 20:37:25 +0200

    pulling the data stored from firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -55,6 +55,21 @@ const config =
   };
 //   end
 
+// fetching the data from the firebase
+export const convertCollectionsSnapshotToMap = (collections)  => {
+	const transformedCollection = collections.docs.map(doc => {
+		const { title, items } = doc.data();
+
+		return {
+			routeName: encodeURI(title.toLowerCase()),
+			id: doc.id,
+			title,
+			items
+		}
+	});
+	console.log(transformedCollection);
+}
+
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
 
--- src/pages/shop/Shop.jsx
+++ src/pages/shop/Shop.jsx
@@ -4,6 +4,9 @@ import CollectionsOverView from '../../components/collections-overview/Collectio
 import CollectionPage from '../collection/Collection';
 //  importing rout
 import { Route } from 'react-router-dom';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
 // remove this after using redux
 // import ShopData from './shopdata';
 //  MOVED THESE 3 INTO CollectionsOverview.jsx
@@ -27,16 +30,31 @@ import { Route } from 'react-router-dom';
 	// }
 
 	// AFTER REDUX
-const ShopPage = ({ match }) => (
-	<div className="shop-page">
-		<Route exact path={`${match.path}`} component={CollectionsOverView} />
-		<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-		{/*  MOVED INTO CollectionsOverview.jsx */}
-		{/* {collections.map(({ id, ...otherCollectionsProps}) => (
-				<CollectionPreview key={id} {...otherCollectionsProps} />
-			))} */}
-	</div>
-);
+class ShopPage extends React.Component {
+	unsubscribeFromSnapshot = null;
+
+	componentDidMount() {
+		const collectionRef = firestore.collection("collections");
+		collectionRef.onSnapshot(async snapshot => {
+			convertCollectionsSnapshotToMap(snapshot)
+		});
+	}
+
+	render() {
+		const { match} = this.props
+		return (
+			<div className="shop-page">
+				<Route exact path={`${match.path}`} component={CollectionsOverView} />
+				<Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+				{/*  MOVED INTO CollectionsOverview.jsx */}
+				{/* {collections.map(({ id, ...otherCollectionsProps}) => (
+						<CollectionPreview key={id} {...otherCollectionsProps} />
+					))} */}
+			</div>
+		)
+	}
+}
+
 
 //  MOVED TO CollectionsOverView.jsx
 // const mapStateToProps = createStructuredSelector({
